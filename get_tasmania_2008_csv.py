fb9548ddcf29bd8c949f2f08d2480b095581c930
Harvey Wang <harvey@dal.ca>
2021-02-16 12:36:17 -0400

    renamed csv to training and testing sets

--- get_tasmania_2008_csv.py
+++ get_tasmania_2008_csv.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-PUBLIC_CSV_NAME = '~/Desktop/BENTHOZ_2015/BENTHOZ_2015_PUBLIC.csv'
-TASMANIA_CSV_NAME = '~/Desktop/shields-bnn/tasmania-2008.csv'
+PUBLIC_CSV_NAME = '~/Desktop/BENTHOZ_2015/BENTHOZ_2015_SECRET.csv'
+TASMANIA_CSV_NAME = '~/Desktop/shields-bnn/tasmania-testing.csv'
 CAMPAIGN_COL = 'campaign'
 TASMANIA_CAMPAIGN = 'Tasmania 2008'
 
-public_df = pd.read_csv(PUBLIC_CSV_NAME)
-public_df.loc[public_df[CAMPAIGN_COL] == TASMANIA_CAMPAIGN].to_csv(
+df = pd.read_csv(PUBLIC_CSV_NAME)
+df.loc[df[CAMPAIGN_COL] == TASMANIA_CAMPAIGN].to_csv(
     TASMANIA_CSV_NAME, index = False,
 )
